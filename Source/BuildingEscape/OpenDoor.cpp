d47f92cf858dcd94ac7ecddf018617d0131b42bc
Albert Koch <albert.koch89@gmail.com>
2023-10-10 22:13:59 +0200

    Added Method to rotate door by 90 degrees.

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,41 @@
+// Copyright Albert Koch 2023
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	this->Rotate();
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UOpenDoor::Rotate()
+{
+	FRotator rotation = GetOwner()->GetActorRotation();
+	rotation.Yaw += 90.f;
+	GetOwner()->SetActorRotation(rotation);
+}
+
